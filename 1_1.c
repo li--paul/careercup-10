073349d1ccd81ec70c83cc63afac01a3725e2206
parker0203 <parker0203@gmail.com>
2012-07-12 00:21:41 -0400

    add quick_sort

--- 1_1.c
+++ 1_1.c
@@ -29,6 +29,35 @@ int hash_table(char *string)
 	return 0;
 }
 
+void swap(char *ch1, char *ch2){
+	char temp = *ch1;
+	*ch1 = *ch2;
+	*ch2 = temp;
+}
+
+int quick_sort(char *str, int left, int right){
+	if(left < right){
+		char pivot = str[left];
+		int i = left;
+		int j = i + 1;
+
+		for( ; j <= right; ++j){
+			if(pivot > str[j]){
+				swap(&str[i + 1], &str[j]);
+				++i;
+			} 
+
+		if(pivot == str[j])
+		return 1;
+		} 
+		
+		swap(&str[left], &str[i]);
+		if(quick_sort(str, left, i - 1)) return 1;
+		if(quick_sort(str, i + 1, right)) return 1;
+	}
+	return 0;
+}
+
 int main(int argc, char **argv)
 {
 	if(argc != 2){
@@ -36,8 +65,10 @@ int main(int argc, char **argv)
 		return 0;
 	}
 //	int result = brute_force(argv[1]);
-	int result = hash_table(argv[1]);
+//	int result = hash_table(argv[1]);
+	int result = quick_sort(argv[1], 0, strlen(argv[1]) - 1);
 	printf("%d\n", result);
+//	printf("%s\n", argv[1]);
 
 	return 0;
 }
