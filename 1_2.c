2c0126e476a62f467ae8a5a8f88ef48541fde725
parker0203 <parker0203@gmail.com>
2012-07-15 00:09:13 -0400

    my version, know why *str = hello can't revise

--- 1_2.c
+++ 1_2.c
@@ -2,37 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *reverse_str(char *str){
+void reverse_str(char *str){
 	char temp;
-	size_t left, right;
-	size_t lenth = strlen(str);
+	char *end = str;
+	end +=(int)strlen(str) - 1;
 
-	left = 0;
-	right = lenth -1;
-
-	while(left < right){
-		temp = str[left];
-		str[left++] = str[right];
-		str[right--] = temp;
+	while(str < end){
+		temp = *str;
+		*str++ = *end;
+		*end-- = temp;
 	}
-//	printf("%d", (int)strlen(str));
-//	end +=(int)strlen(str) - 1;
-//	printf("%s", end);
-//	int i = 4;
-//	while(i--){
-//		temp = str[0];
-//		str = (*end);
-//		*end-- = temp;
-//	}
-	return str;
-
 }
 
-int main(int argc, char **argv){
-	char *result = reverse_str(argv[1]);
-//	char *str = "hello";
-//	char *result = "abcde";
-//	result = reverse_str(str);
-	printf("%s\n", result);
+int main(){
+
+	char str[] ="hello";
+	reverse_str(str);
+	printf("%s\n", str);
 	return 0;
 }
