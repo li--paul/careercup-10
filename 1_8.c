43c7cde7c5dbdc0bcec0617a5eb1dddda3b2e2c8
parker0203 <parker0203@gmail.com>
2012-07-18 23:06:41 -0400

    isRotation using malloc

--- 1_8.c
+++ 1_8.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <malloc.h>
 
 int isSubstring(char *str1, char *str2){
 
@@ -17,14 +18,34 @@ int isSubstring(char *str1, char *str2){
 	return j == len2;
 }
 
+int isRotation(char *str1, char *str2){
+
+	int len1 = strlen(str1);
+	int len2 = strlen(str2);
+		printf("%s\n", str2);
+	
+	if(len1 == len2 && len1 > 0){
+		char *strtmp = (char*)malloc(sizeof(char) * len1 * 2);
+		strcpy(strtmp, str1);
+		strcat(strtmp, str1);
+		printf("%s\n", str2);
+		printf("%s\n", strtmp);
+		return isSubstring(strtmp, str2);
+	}
+	return 0;
+}
+
 int main(){
 
-	char str1[] = "helloworldmynameisparker0203";
-	char str2[] = "mynamke";
+	char str1[] = "helloworld";
+	char str2[] = "oworldhell";
 
-	int isSub = isSubstring(str1, str2);
+	int isRot = isRotation(str1, str2);
+//	int isSub = isSubstring(str1, str2);
 
-	printf("%d\n", isSub);
+	printf("%d\n", isRot);
+
+//	printf("%d\n", isSub);
 
 	return 0;
 }
