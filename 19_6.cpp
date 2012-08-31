931ded9a12defd0faa5f6aeadbb5a8d4094d7b51
parker0203 <parker0203@gmail.com>
2012-08-31 18:21:53 -0400

    print num in en

--- 19_6.cpp
+++ 19_6.cpp
@@ -9,7 +9,17 @@
 
 #include <iostream>
 
+
 using namespace std;
+
+
+string word1[] = {"", "One ", "Two ", "Three ", "Four ", "Five", 
+						"Six ", "Seven ", "Eight ", "Nine ", 
+						"Ten ", "Eleven ", "Twelve ", "Thirteen ", "Fourteen ", "Fifteen", 
+						"Sixteen ", "Seventeen ", "Eighteen ", "Nineteen "};
+string word10[] = {"", "Ten ", "Twenty ", "Thirty ", "Forty ", "Fifty ", 
+							"Sixty ", "Seventy ", "Eighty ", "Ninety "};
+string word1000[] = {"", "Thousand ", "Million "};
 
 
 void print_num(int num, int level){
@@ -21,13 +31,6 @@ void print_num(int num, int level){
 		return;
 	}
 
-	string word1[] = {"", "One ", "Two ", "Three ", "Four ", "Five", 
-						"Six ", "Seven ", "Eight ", "Nine ", 
-						"Ten ", "Eleven ", "Twelve ", "Thirteen ", "Fourteen ", "Fifteen", 
-						"Sixteen ", "Seventeen ", "Eighteen ", "Nineteen "};
-	string word10[] = {"", "Ten ", "Twenty ", "Thirty ", "Forty ", "Fifty ", 
-							"Sixty ", "Seventy ", "Eighty ", "Ninety "};
-	string word1000[] = {"", "Thousand ", "Million "};
 
 	int upper;
 	int lower;
@@ -41,12 +44,14 @@ void print_num(int num, int level){
 	if(temp > 0){
 		cout << word1[temp] << "Hundred and ";
 	}
+
 	lower -= temp * 100;
 	if(lower < 20){
 		cout << word1[lower];
 	}else{
 		cout << word10[lower / 10] << word1[lower % 10];
 	}
+
 	if(level > 0){
 		cout << word1000[level] << ", ";
 	}else{
